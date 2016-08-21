63f3d9d4efb862af9821184235d21b5123d7eadd
Olle Kelderman <omkelderman@gmail.com>
2016-08-21 17:34:09 +0200

    wop

---
+++ SuperSimpleNetworkChat-windows/ChatMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSimpleNetworkChat_windows
+{
+    public abstract class ChatMessage
+    {
+        public string Message { set; get; }
+
+    }
+
+    public class SelfChatMessage : ChatMessage
+    {
+        public List<string> MessageReadPeers { get; } = new List<string>();
+
+        public int MessageReadCount => MessageReadPeers.Count;
+        public string MessageReadPeersList => string.Join("\n", MessageReadPeers);
+    }
+
+    public class PeerChatMessage : ChatMessage
+    {
+        public string Name { set; get; }
+
+    }
+}
--- SuperSimpleNetworkChat-windows/MainWindow.xaml.cs
+++ SuperSimpleNetworkChat-windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -22,6 +23,32 @@ namespace SuperSimpleNetworkChat_windows
         public MainWindow()
         {
             InitializeComponent();
+
+            List<ChatMessage> chatMessages = new List<ChatMessage>
+            {
+                new PeerChatMessage {Name = "peer 1", Message = "message1"},
+                new PeerChatMessage {Name = "peer 2", Message = "message2"},
+                new SelfChatMessage {Message = "message3", MessageReadPeers = { "A", "B", "C"}},
+                new PeerChatMessage {Name = "peer 3", Message = "message4"},
+            };
+
+            ChatBox.ItemsSource = chatMessages;
+        }
+    }
+
+    public class MockChatMessages
+    {
+        public ObservableCollection<ChatMessage> Items { get; set; }
+
+        public MockChatMessages()
+        {
+            Items = new ObservableCollection<ChatMessage>()
+            {
+                new PeerChatMessage {Name = "*peer 1", Message = "*message1"},
+                new PeerChatMessage {Name = "*peer 2", Message = "*message2"},
+                new SelfChatMessage {Message = "*message3", MessageReadPeers = { "*a", "*b", "*c"}},
+                new PeerChatMessage {Name = "*peer 3", Message = "*message4"},
+            };
         }
     }
 }
